dab8260a2dbd59ef83c3effdc2dca900884c5fb8
Roger M <rsmoraldo2@gmail.com>
2018-07-17 11:47:27 -0400

    removed console.log

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var library = Array.from(sacredIcon.children)
 var haloArray = document.getElementsByTagName("article")
 var theArk = Array.from(haloArray)
 var theIndex
+var deltaIcon = document.querySelectorAll(".tile")
+var deltaLibrary = Array.from(deltaIcon)
 
 library.forEach(el => {
   el.addEventListener("click", () => {
     theIndex = library.findIndex(index => index == el)
-    console.log(theIndex)
     if (el.classList.contains("current")) {
       el.classList.remove("current")
       theArk.forEach(elem => {
@@ -22,6 +23,19 @@ library.forEach(el => {
       })
       el.classList.add("current")
       haloArray[theIndex].classList.add("current")
+    }
+  })
+})
+
+deltaLibrary.forEach(el => {
+  el.addEventListener("click", () => {
+    if (el.classList.contains("highlight")) {
+      el.classList.remove("highlight")
+    } else {
+      library.forEach(ele => {
+        ele.classList.remove("highlight")
+      })
+      el.classList.add("highlight")
     }
   })
 })
